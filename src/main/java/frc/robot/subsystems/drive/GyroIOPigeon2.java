71164d42f27979da37fef6ae398a1e42c7f52b5d
jamartiney <56280907+jamartiney@users.noreply.github.com>
2026-02-16 19:21:26 -0500

    Initial Check-In of Demi Bot 386 for WPILIB 2025

---
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -0,0 +1,19 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot.subsystems.drive;
+
+import static frc.robot.subsystems.drive.DriveConstants.*;
+
+/** IO implementation for Pigeon 2. */
+public class GyroIOPigeon2 implements GyroIO {
+
+  public GyroIOPigeon2() {}
+
+  @Override
+  public void updateInputs(GyroIOInputs inputs) {}
+}
